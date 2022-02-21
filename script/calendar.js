20d839a18825a2738c08bc2fc26304d775cec72a
Mint1832 <mint18032@gmail.com>
2022-02-22 00:10:46 +0700

    Fix menuBtn.

--- script/calendar.js
+++ script/calendar.js
@@ -6,4 +6,9 @@ document.getElementById("year").innerHTML = date.getFullYear();
 
 function myFunction(x) {
     x.classList.toggle("change");
+    if (document.getElementById('sidebar').style.width == '80%') {
+        document.getElementById('sidebar').style.width = '0';
+    } else {
+        document.getElementById('sidebar').style.width = '80%';
+    }
 }
